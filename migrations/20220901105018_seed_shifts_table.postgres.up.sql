ac5e466d8b9694bbaf7ed16128ff6265b954ec6d
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2022-09-01 12:55:22 +0200

    insert into shifts

---
+++ migrations/20220901105018_seed_shifts_table.postgres.up.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.shifts
+( code, name,created_at, updated_at)
+VALUES('D', 'Dnevna','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
+INSERT INTO public.shifts
+( code, name,created_at, updated_at)
+VALUES('N', 'Nocna','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
