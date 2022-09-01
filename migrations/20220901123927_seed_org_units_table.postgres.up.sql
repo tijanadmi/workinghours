dafd18d270ce37c6c67027bbeb17fe42f442897a
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2022-09-01 15:02:56 +0200

    not null

---
+++ migrations/20220901123927_seed_org_units_table.postgres.up.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.org_units
+( code, name,created_at, updated_at)
+VALUES('50006380', 'RDC Centar - Beograd','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
+INSERT INTO public.org_units
+( code, name,created_at, updated_at)
+VALUES('50006381', 'Sližba RDC Bor','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
+INSERT INTO public.org_units
+( code, name,created_at, updated_at)
+VALUES('50006382', 'Služba RDC Valjevo','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
+INSERT INTO public.org_units
+( code, name,created_at, updated_at)
+VALUES('50006383', 'RDC Sever - Novi Sad','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
+INSERT INTO public.org_units
+( code, name,created_at, updated_at)
+VALUES('50006384', 'RDC Jug - Kruševac','2022-09-01 00:00:00.000', '2022-09-01 00:00:00.000');
